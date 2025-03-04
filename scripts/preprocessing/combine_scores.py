7e7523310242d24f2018647be6b068915e19b95d
Robin Kokot <robin.edu.hr@gmail.com>
2025-03-04 21:16:49 +0100

    started on combining scores

---
+++ scripts/preprocessing/combine_scores.py
@@ -0,0 +1,36 @@
+import argparse
+import os
+import pandas as pd
+import numpy as np
+from typing import List, Dict
+from pathlib import Path
+
+
+
+def get_numeric_columns(df):
+
+
+
+def add_scores(df, number_cols, function='sum'):
+
+  return df.sum
+
+def normalize_scores(column):
+
+
+def process_file(file_path, output_path, function='sum', column_name='', normalize=False):
+  return Path
+
+def process_folder(input_dir, output_dir, function='sum', column_name='', normalize=False):
+  return folder 
+
+
+
+
+
+def main():
+
+
+
+if __name__ = "__main__":
+  main()
