65f1269470e0e53c55b121a0771ea867f85bf703
rokokot <robin.edu.hr@gmail.com>
2025-04-30 16:36:34 +0200

    merge failed run

--- experiment_runners/glot500_finetune.sh
+++ experiment_runners/glot500_finetune.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 #SBATCH --job-name=finetune_experiments
-#SBATCH --time=24:00:00
+#SBATCH --time=00:30:00
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=4
 #SBATCH --mem=32G
 #SBATCH --gres=gpu:1
-#SBATCH --partition=gpu_a100
+#SBATCH --partition=gpu_a100_debug
 #SBATCH --clusters=wice
 #SBATCH --account=intro_vsc37132
 
